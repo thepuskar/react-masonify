bf12ce90a6bc710fe2af469189ed86850d99538b
Puskar Adhikari <puskar.ad@gmail.com>
2024-10-27 15:46:56 +0545

    feat: useIsMounted hook to check component mount or not

---
+++ lib/hooks/useIsMounted.ts
@@ -0,0 +1,31 @@
+import { useCallback, useEffect, useRef } from "react";
+
+/**
+ * Custom hook that returns a function to check if the component is currently mounted.
+ * This is useful for avoiding memory leaks by checking component mount status
+ * before performing actions like state updates on unmounted components.
+ *
+ * @returns {() => boolean} A callback function that returns `true` if the component is mounted, and `false` otherwise.
+ *
+ * @example
+ * const isMounted = useIsMounted();
+ *
+ * useEffect(() => {
+ *   if (isMounted()) {
+ *     // Perform actions safely knowing the component is mounted
+ *   }
+ * }, []);
+ */
+export function useIsMounted(): () => boolean {
+  const isMounted = useRef(false);
+
+  useEffect(() => {
+    isMounted.current = true;
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  return useCallback(() => isMounted.current, []);
+}
