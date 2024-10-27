62d5fe6a236eed15b998b0ce7ed3d6dc9507bc62
Puskar Adhikari <puskar.ad@gmail.com>
2024-10-27 18:04:23 +0545

    chore: type exported

--- lib/main.tsx
+++ lib/main.tsx
@@ -1,5 +1,12 @@
 import Masonry from "./components/Masonry";
 import ResponsiveMasonry from "./components/ResponsiveMasonry";
 
+// Export types
+export type { MasonryProps } from "./components/Masonry";
+export type {
+  ResponsiveMasonryProps,
+  ColumnsCountBreakPoints,
+} from "./components/ResponsiveMasonry";
+
 export default Masonry;
 export { ResponsiveMasonry };
