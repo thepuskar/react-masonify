a3b93ecd2df31e3c837d385f93a38c805148abf7
Puskar Adhikari <puskar.ad@gmail.com>
2024-10-27 15:19:51 +0545

    feat: masonry component used and render multiple img in masony grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import { useState } from "react";
-import logo from "./logo.svg";
+import Masonry from "@/react-masonify";
 import "./app-style.css";
-import { Hello } from "@/react-masonify";
+import IMG_DATA from "./data.json";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Hello />
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-      </header>
+      <div className="min-h-screen bg-gray-100 p-6">
+        <h1 className="text-4xl font-bold text-center mb-8">
+          React Masonify - Masonry Grid Layout
+        </h1>
+        <div className="max-w-7xl mx-auto">
+          <Masonry gap="1rem">
+            {IMG_DATA?.map((src) => (
+              <ImgCard src={src} key={src} />
+            ))}
+          </Masonry>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const ImgCard = ({ src }: { src: string }) => {
+  return (
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow ">
+      <img src={src} alt=" Scenic" className="rounded-lg" />
+    </div>
+  );
+};
